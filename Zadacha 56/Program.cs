5fb805edeb350b78f0440c130929dbb25d93dedf
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-09-04 16:29:18 +0300

    Задача 56 решено

--- Zadacha 56/Program.cs
+++ Zadacha 56/Program.cs
@@ -9,3 +9,59 @@
 // 5 2 6 7
 
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+Console.WriteLine();
+Console.WriteLine("Например, задан массив:");
+
+int[,] CreateMatrixRnd(int row, int col)//, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)   //row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)   //col
+        {
+            matrix[i, j] = i + j;
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],2}");
+            if (j != matrix.GetLength(1) - 1) Console.Write(",");
+        }
+        Console.Write(" |");
+        Console.WriteLine();
+    }
+}
+int[,] mat = CreateMatrixRnd(3, 5);
+PrintMatrix(mat);
+
+int minSuma = Int32.MaxValue;
+int inRow = 0;
+
+for (int i = 0; i < mat.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < mat.GetLength(1); j++)
+    {
+        sum = sum + mat[i, j];
+    }
+    if (sum < minSuma)
+    {
+        minSuma = sum;
+        inRow++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Строка: " + (inRow) + ", с наименьшей суммой элементов: " + (minSuma));
